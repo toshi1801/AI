35c4cd6d4ffc8e60b88a61bd0fd624e2da9bc1ad
najain <naman.jain1801@gmail.com>
2018-09-24 01:15:28 -0400

    Final Draft.

--- HW1/npuzzle.py
+++ HW1/npuzzle.py
@@ -6,10 +6,11 @@ for solving the n-Puzzle, which is a generalization of the 8 and 15 puzzle to
 squares of arbitrary size (we will only test it with 8-puzzles for now). 
 See Courseworks for detailed instructions.
 
-@author: YOUR NAME (YOUR UNI)
+@author: Naman Jain (nj2387)
 """
 
 import time
+
 
 def state_to_string(state):
     row_strings = [" ".join([str(cell) for cell in row]) for row in state]
@@ -50,9 +51,24 @@ def get_successors(state):
 
     # YOUR CODE HERE . Hint: Find the "hole" first, then generate each possible
     # successor state by calling the swap_cells method.
-    # Exclude actions that are not applicable. 
+    # Exclude actions that are not applicable.
+    i = -1
+    j = -1
+    for s in state:
+        i = state.index(s)
+        if 0 in s:
+            j = s.index(0)
+            break
 
-    
+    if (j + 1) < len(state):
+        child_states.append(tuple(['Left', swap_cells(state, i, j, i, j + 1)]))
+    if (j - 1) >= 0:
+        child_states.append(tuple(['Right', swap_cells(state, i, j, i, j - 1)]))
+    if (i + 1) < len(state):
+        child_states.append(tuple(['Up', swap_cells(state, i, j, i + 1, j)]))
+    if (i - 1) >= 0:
+        child_states.append(tuple(['Down', swap_cells(state, i, j, i - 1, j)]))
+
     return child_states
 
             
@@ -61,8 +77,22 @@ def goal_test(state):
     Returns True if the state is a goal state, False otherwise. 
     """    
 
-    #YOUR CODE HERE
-    return False # Replace this 
+    # YOUR CODE HERE
+    n = len(state)
+    goal_states = []
+    for st in state:
+        goal_states.extend([s for s in st])
+
+    if len(goal_states) != (n * n):
+        return False
+
+    i = 0
+    for gs in goal_states:
+        if gs != i:
+            return False
+        i += 1
+
+    return True
 
    
 def bfs(state):
@@ -81,11 +111,42 @@ def bfs(state):
     fringe = []
     closed = set()
     parents = {}
-    #YOUR CODE HERE
 
-    #  return solution, states_expanded, max_fringe
-    return None, states_expanded, max_fringe # No solution found
-                               
+    # YOUR CODE HERE
+    fringe.append(state)
+    parents[state] = (None, None)
+
+    while True:
+
+        if max_fringe < len(fringe):
+            max_fringe = len(fringe)
+
+        if not fringe:
+            return None, states_expanded, max_fringe
+
+        state_to_expand = fringe.pop(0)
+
+        if goal_test(state_to_expand):
+            return extract_solution(state_to_expand, parents), states_expanded, max_fringe
+
+        if state_to_expand not in closed:
+            states_expanded += 1
+            new_states = get_successors(state_to_expand)
+            closed.add(state_to_expand)
+            for s in new_states:
+                if s[1] not in closed:
+                    fringe.append(s[1])
+                    parents[s[1]] = (state_to_expand, s[0])
+
+
+def extract_solution(final_state, parents):
+    final_solution = []
+    state = final_state
+    while parents[state][0]:
+        final_solution.insert(0, parents[state][1])
+        state = parents[state][0]
+    return final_solution
+
      
 def dfs(state):
     """
@@ -103,10 +164,32 @@ def dfs(state):
     fringe = []
     closed = set()
     parents = {}
-    #YOUR CODE HERE
 
-    #  return solution, states_expanded, max_fringe
-    return None, states_expanded, max_fringe # No solution found
+    # YOUR CODE HERE
+    fringe.append(state)
+    parents[state] = (None, None)
+
+    while True:
+
+        if max_fringe < len(fringe):
+            max_fringe = len(fringe)
+
+        if not fringe:
+            return None, states_expanded, max_fringe
+
+        state_to_expand = fringe.pop()
+
+        if goal_test(state_to_expand):
+            return extract_solution(state_to_expand, parents), states_expanded, max_fringe
+
+        if state_to_expand not in closed:
+            states_expanded += 1
+            new_states = get_successors(state_to_expand)
+            closed.add(state_to_expand)
+            for s in new_states:
+                if s[1] not in closed:
+                    fringe.append(s[1])
+                    parents[s[1]] = (state_to_expand, s[0])
 
 
 def misplaced_heuristic(state):
@@ -114,8 +197,16 @@ def misplaced_heuristic(state):
     Returns the number of misplaced tiles.
     """
 
-    #YOUR CODE HERE
-    return 0 # replace this
+    # YOUR CODE HERE
+    i = 0
+    misplaced_count = 0
+    for st in state:
+        for s in st:
+            if i and s != i:
+                misplaced_count += 1
+            i += 1
+
+    return misplaced_count
 
 
 def manhattan_heuristic(state):
@@ -124,7 +215,25 @@ def manhattan_heuristic(state):
     position and the goal position. Then return the sum of all distances.
     """
 
-    return 0 # replace this
+    i = 0
+    tiles_position = {}
+    tiles_expected_position = {}
+    tile_value = 0
+    for st in state:
+        j = 0
+        for s in st:
+            tiles_position[s] = (i, j)
+            tiles_expected_position[tile_value] = (i, j)
+            tile_value += 1
+            j += 1
+        i += 1
+
+    manhattan_distance = 0
+    for k, v in tiles_position.items():
+        if k:
+            manhattan_distance += abs(v[0] - tiles_expected_position[k][0]) + abs(v[1] - tiles_expected_position[k][1])
+
+    return manhattan_distance
 
 
 def best_first(state, heuristic):
@@ -148,9 +257,31 @@ def best_first(state, heuristic):
     closed = set()
     parents = {}
 
-    #YOUR CODE HERE
+    # YOUR CODE HERE
+    heappush(fringe, (heuristic(state), state))
+    parents[state] = (None, None)
 
-    return None, states_expanded, max_fringe # No solution found
+    while True:
+
+        if max_fringe < len(fringe):
+            max_fringe = len(fringe)
+
+        if not fringe:
+            return None, states_expanded, max_fringe
+
+        state_to_expand = heappop(fringe)[1]
+
+        if goal_test(state_to_expand):
+            return extract_solution(state_to_expand, parents), states_expanded, max_fringe
+
+        if state_to_expand not in closed:
+            states_expanded += 1
+            new_states = get_successors(state_to_expand)
+            closed.add(state_to_expand)
+            for s in new_states:
+                if s[1] not in closed:
+                    heappush(fringe, (heuristic(s[1]), s[1]))
+                    parents[s[1]] = (state_to_expand, s[0])
 
 
 def astar(state, heuristic):
@@ -175,9 +306,33 @@ def astar(state, heuristic):
     parents = {}
     costs = {}
 
-    #YOUR CODE HERE
+    # YOUR CODE HERE
+    costs[state] = 0
+    heappush(fringe, (heuristic(state) + costs[state], state))
+    parents[state] = (None, None)
 
-    return None, states_expanded, max_fringe # No solution found
+    while True:
+
+        if max_fringe < len(fringe):
+            max_fringe = len(fringe)
+
+        if not fringe:
+            return None, states_expanded, max_fringe
+
+        state_to_expand = heappop(fringe)[1]
+
+        if goal_test(state_to_expand):
+            return extract_solution(state_to_expand, parents), states_expanded, max_fringe
+
+        if state_to_expand not in closed:
+            states_expanded += 1
+            new_states = get_successors(state_to_expand)
+            closed.add(state_to_expand)
+            for s in new_states:
+                if s[1] not in closed:
+                    costs[s[1]] = costs[state_to_expand] + 1
+                    heappush(fringe, (heuristic(s[1]) + costs[s[1]], s[1]))
+                    parents[s[1]] = (state_to_expand, s[0])
 
 
 def print_result(solution, states_expanded, max_fringe):
@@ -192,60 +347,67 @@ def print_result(solution, states_expanded, max_fringe):
     print("Max fringe size: {}.".format(max_fringe))
 
 
-
 if __name__ == "__main__":
 
-    #Easy test case
+    # Easy test case
     test_state = ((1, 4, 2),
-                  (0, 5, 8), 
-                  (3, 6, 7))  
+                  (0, 5, 8),
+                  (3, 6, 7))
 
-    #More difficult test case
-    #test_state = ((7, 2, 4),
-    #              (5, 0, 6), 
-    #              (8, 3, 1))  
+    # More difficult test case
+    # test_state = ((7, 2, 4),
+    #               (5, 0, 6),
+    #               (8, 3, 1))
 
     print(state_to_string(test_state))
     print()
 
     print("====BFS====")
     start = time.time()
-    solution, states_expanded, max_fringe = bfs(test_state) #
-    end = time.time() 
+    solution, states_expanded, max_fringe = bfs(test_state)
+    end = time.time()
     print_result(solution, states_expanded, max_fringe)
     if solution is not None:
         print(solution)
     print("Total time: {0:.3f}s".format(end-start))
 
-    #print() 
-    #print("====DFS====") 
-    #start = time.time()
-    #solution, states_expanded, max_fringe = dfs(test_state)
-    #end = time.time()
-    #print_result(solution, states_expanded, max_fringe)
-    #print("Total time: {0:.3f}s".format(end-start))
+    print()
+    print("====DFS====")
+    start = time.time()
+    solution, states_expanded, max_fringe = dfs(test_state)
+    end = time.time()
+    print_result(solution, states_expanded, max_fringe)
+    if solution is not None:
+        print(solution)
+    print("Total time: {0:.3f}s".format(end-start))
 
-    #print() 
-    #print("====Greedy Best-First (Misplaced Tiles Heuristic)====") 
-    #start = time.time()
-    #solution, states_expanded, max_fringe = best_first(test_state, misplaced_heuristic)
-    #end = time.time()
-    #print_result(solution, states_expanded, max_fringe)
-    #print("Total time: {0:.3f}s".format(end-start))
+    print()
+    print("====Greedy Best-First (Misplaced Tiles Heuristic)====")
+    start = time.time()
+    solution, states_expanded, max_fringe = best_first(test_state, misplaced_heuristic)
+    end = time.time()
+    print_result(solution, states_expanded, max_fringe)
+    if solution is not None:
+        print(solution)
+    print("Total time: {0:.3f}s".format(end-start))
     
-    #print() 
-    #print("====A* (Misplaced Tiles Heuristic)====") 
-    #start = time.time()
-    #solution, states_expanded, max_fringe = astar(test_state, misplaced_heuristic)
-    #end = time.time()
-    #print_result(solution, states_expanded, max_fringe)
-    #print("Total time: {0:.3f}s".format(end-start))
+    print()
+    print("====A* (Misplaced Tiles Heuristic)====")
+    start = time.time()
+    solution, states_expanded, max_fringe = astar(test_state, misplaced_heuristic)
+    end = time.time()
+    print_result(solution, states_expanded, max_fringe)
+    if solution is not None:
+        print(solution)
+    print("Total time: {0:.3f}s".format(end-start))
 
-    #print() 
-    #print("====A* (Total Manhattan Distance Heuristic)====") 
-    #start = time.time()
-    #solution, states_expanded, max_fringe = astar(test_state, manhattan_heuristic)
-    #end = time.time()
-    #print_result(solution, states_expanded, max_fringe)
-    #print("Total time: {0:.3f}s".format(end-start))
+    print()
+    print("====A* (Total Manhattan Distance Heuristic)====")
+    start = time.time()
+    solution, states_expanded, max_fringe = astar(test_state, manhattan_heuristic)
+    end = time.time()
+    print_result(solution, states_expanded, max_fringe)
+    if solution is not None:
+        print(solution)
+    print("Total time: {0:.3f}s".format(end-start))
 
